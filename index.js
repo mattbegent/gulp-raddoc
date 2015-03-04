2a536fcae4791d0de04e6d2ef19499ad0c32fbe4
Matt <matt@mattbegent.co.uk>
2015-03-04 14:48:12 +0000

    Minor CSS tweaks and code formatting

--- index.js
+++ index.js
@@ -14,72 +14,72 @@ var comments = '';
 
 module.exports = function(options) {
 
-	options = _.extend({
-		css: '../components/userstyle.css',
+    options = _.extend({
+        css: '../components/userstyle.css',
         outputFolder: './',
-		docHomeTemplate: 'index.ejs',
-		docHomeOutput: 'index.html',
-		docTitle: 'Rad Doc'
+        docHomeTemplate: 'index.ejs',
+        docHomeOutput: 'index.html',
+        docTitle: 'Rad Doc'
     }, options || {});
 
-	comments = ''; // clear
-	
-	return through.obj(function (file, enc, cb) {
+    comments = ''; // clear
+    
+    return through.obj(function (file, enc, cb) {
 
-	    if (file.isNull()) {
-	      cb(null, file);
-	    }
+        if (file.isNull()) {
+          cb(null, file);
+        }
 
-	    if (['.css', '.less', '.scss'].indexOf(path.extname(file.path)) === -1) {
+        if (['.css', '.less', '.scss'].indexOf(path.extname(file.path)) === -1) {
             this.emit('error', new PluginError(PLUGIN_NAME,'CSS only currently'));
             cb(null, file);
-	    }
+        }
 
-	    if (file.isStream()) {
-	        this.emit('error', new PluginError(PLUGIN_NAME, 'Streams are not supported'));
-	        cb(null, file);
-	    }
+        if (file.isStream()) {
+            this.emit('error', new PluginError(PLUGIN_NAME, 'Streams are not supported'));
+            cb(null, file);
+        }
 
-	    if (file.isBuffer()) {
-	    	getComments(file.contents, options);
-		}
+        if (file.isBuffer()) {
+            getComments(file.contents, options);
+        }
 
-	    cb(null, file);
+        cb(null, file);
 
-	}, function (cb) {
+    }, function (cb) {
 
-	    renderTemplate(comments, options);
+        renderTemplate(comments, options);
 
-	    cb();
+        cb();
 
-	});
+    });
 
 };
 
 function getComments(contents, options) {
 
-	var currentContent = String(contents);
+    var currentContent = String(contents);
 
-	if(currentContent.indexOf("*/") > -1) {
+    if(currentContent.indexOf("*/") > -1) {
 
-		var currentComment = currentContent.split('*/')[0].replace('/*', '');
+        var currentComment = currentContent.split('*/')[0].replace('/*', '');
 
-		// turn into markdown
-		currentComment = marked(currentComment);
+        // turn into markdown
+        currentComment = marked(currentComment);
 
-	    // get html example
-		currentComment = "<div class='radDoc-Section'>" + currentComment + "</div>";
-		var getHTML = unescapeHTML(currentComment.substring(currentComment.lastIndexOf("<pre><code>")+11,currentComment.lastIndexOf("</code></pre>")));
-		currentComment = insertBeforeLastOccurrence(currentComment, '<pre>', getHTML);
+        // get html example
+        currentComment = "<div class='radDoc-Section'>" + currentComment + "</div>";
+        var getHTML = unescapeHTML(currentComment.substring(currentComment.lastIndexOf("<pre><code>")+11,currentComment.lastIndexOf("</code></pre>")));
+        currentComment = insertBeforeLastOccurrence(currentComment, '<pre>', getHTML);
 
-		comments += currentComment;
-		
-	}
+        comments += currentComment;
+        
+    }
 
 }
 
 function unescapeHTML(escapedHTML) {
-	return escapedHTML.replace(/&lt;/g,'<').replace(/&gt;/g,'>').replace(/&amp;/g,'&').replace(/&quot;/g,'"');
+    return escapedHTML.replace(/&lt;/g,'<').replace(/&gt;/g,'>').replace(/&amp;/g,'&').replace(/&quot;/g,'"');
 }
 
 function insertBeforeLastOccurrence(strToSearch, strToFind, strToInsert) {
@@ -96,17 +96,17 @@ function renderTemplate(comments,options) {
         docTitle: options.docTitle
     };
   
-	 fs.readFile(path.resolve(__dirname, options.docHomeTemplate), 'utf8', function (err, data) {
-	  	if (err) throw new PluginError(PLUGIN_NAME, err);
-	 	var template = ejs.render(data, templateData);
-	 	fs.writeFile(path.resolve(__dirname, options.docHomeOutput), template, function (err) {
-		    if (err) throw new PluginError(PLUGIN_NAME, err);
+     fs.readFile(path.resolve(__dirname, options.docHomeTemplate), 'utf8', function (err, data) {
+        if (err) throw new PluginError(PLUGIN_NAME, err);
+        var template = ejs.render(data, templateData);
+        fs.writeFile(path.resolve(__dirname, options.docHomeOutput), template, function (err) {
+            if (err) throw new PluginError(PLUGIN_NAME, err);
 
             // copy file to output directory
-		    fs.copy(path.resolve(__dirname, options.docHomeOutput), options.outputFolder + 'raddoc.html', function (err) {
-		        if (err) throw new PluginError(PLUGIN_NAME, err);
-		    });
-		}); 
-	});
+            fs.copy(path.resolve(__dirname, options.docHomeOutput), options.outputFolder + 'raddoc.html', function (err) {
+                if (err) throw new PluginError(PLUGIN_NAME, err);
+            });
+        }); 
+    });
  
 }
